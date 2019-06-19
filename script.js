4999a6e98f491cef205f91a98f7b0969fbc239b8
dbullard2 <spacegeek84@gmail.com>
2019-06-19 14:52:13 -0500

    Completed slideshow for modal

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function showDiv() {
-  //document.getElementsByClassName('left-modal-button').style.backgroundColor = red;
-  alert("Hello! I am an alert box!!");
+var x = 0;
+
+function nextSlide() {
+  if (x == 0) {
+    document.getElementById('slide-one').style.display = "none";
+    document.getElementById('slide-two').style.display = "block";
+    x++;
+  } else if(x == 1){
+    document.getElementById('slide-two').style.display = "none";
+    document.getElementById('slide-three').style.display = "block";
+    x++;
+  } else if(x == 2){
+    document.getElementById('slide-three').style.display = "none";
+    document.getElementById('slide-four').style.display = "block";
+    x++;
+  } else if(x == 3){
+    document.getElementById('slide-four').style.display = "none";
+    document.getElementById('slide-five').style.display = "block";
+    document.getElementById('right-modal-button').textContent = "Finish";
+  }
 }
